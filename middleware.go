ce4d1d30980b56fa9690d4add8a57d92d25ca22f
Zkrallah <muhammad.heshamyt@gmail.com>
2025-05-04 00:05:32 +0300

    fix: Reverted global middleware support for doing it later.

--- middleware.go
+++ middleware.go
@@ -60,11 +60,9 @@ func applyMiddleware(finalHandler Handler, router *Router) Handler {
 		var next func()
 		next = func() {
 
-			// If there are still middlewares to check in the app's middlewares
-			// Combine global middlewares and router-specific middlewares
-			allMiddlewares := append(app.middlewares, router.middlewares...)
-			if currentMiddlewareIdx < len(allMiddlewares) {
-				middlewareWrapper := allMiddlewares[currentMiddlewareIdx]
+			// TODO: Global middleware support, not only router's middlewares
+			if currentMiddlewareIdx < len(router.middlewares) {
+				middlewareWrapper := router.middlewares[currentMiddlewareIdx]
 
 				// Increment the middleware index to check the next middleware in the next
 				// recursive call
